563bec142b9a2fc3a90495089d48c157958f6e45
Marcos Lima <araujolima728@gmail.com>
2025-12-08 22:40:46 -0300

    Add: Projeto doacao  de sangue

---
+++ ControleDoacaoSangue/ControleDoacao/src/poov/dao/ConexaoFactory.java
@@ -0,0 +1,16 @@
+package poov.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+
+public class ConexaoFactory {
+    
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+
+    public static Connection getConexao() {
+
+    }
+}
